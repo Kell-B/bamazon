62a8693dfdd641099551261dcf165f50311dd837
Kell Banks <mckellbanks@gmail.com>
2020-05-11 16:59:55 -0600

    created js file, installed all needed npm pakages, created connection to mysql pro

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err, res) {
+    if (err) throw err;
+    console.log(res);
+});
+
+
